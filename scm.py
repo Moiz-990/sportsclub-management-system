c883ab118dc262f118e384633ee3a09ff30590bf
Moiz-990 <moizmurshad@eng.rizvi.edu.in>
2026-09-11 10:32:23 +0530

    v1.0Update scm.py

--- scm.py
+++ scm.py
@@ -1,1 +1,11 @@
 
+# Sports Club Management System - Version 1.0
+# Features: member registration and membership details
+
+def register_member(member_id, name, sport):
+    print("Member", name, "registered for", sport)
+
+def show_membership(member_id, name, sport):
+    print("Member ID:", member_id)
+    print("Name:", name)
+    print("Sport:", sport)
